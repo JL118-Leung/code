090b4f9eeb4aea9b86746fc72578f7d035fef358
JL118-Leung <jasonleungkit123@protonmail.com>
2025-09-02 14:44:05 +0800

    Update Kaggle1 Cafe Sales.py

--- Kaggle1 Cafe Sales.py
+++ Kaggle1 Cafe Sales.py
@@ -1,29 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import pandas as pd
+
 df = pd.read_csv(r"C:\Users\jason\Downloads\dirty_cafe_sales.csv")
-
-
-# In[22]:
-
-
-df.head(50)
+#Data Cleaning
 df=df.replace('UNKNOWN', '')
 df=df.replace('ERROR', '')
 df=df.fillna('')
 
-
-# In[23]:
-
-
-df.head(20)
-
-
-# In[ ]:
 
 
 
