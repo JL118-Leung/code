08dd79b9694895be4585642228f62b4942f16fe7
JL118-Leung <jasonleungkit123@protonmail.com>
2025-09-10 21:38:07 +0800

    Update Kaggle1 Cafe Sales.py

--- Kaggle1 Cafe Sales.py
+++ Kaggle1 Cafe Sales.py
@@ -6,7 +6,11 @@ df=df.replace('UNKNOWN', '')
 df=df.replace('ERROR', '')
 df=df.fillna('')
 
-
+#convert data types
+df[['Quantity','Price Per Unit','Total Spent']]=df[['Quantity','Price Per Unit','Total Spent']].replace('','0')
+df['Quantity']=df['Quantity'].astype(int)
+df['Price Per Unit']=df['Price Per Unit'].astype(float)
+df['Total Spent']=df['Total Spent'].astype(float)
 
 
 
